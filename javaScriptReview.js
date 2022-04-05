afe06c50660151584b10cbb99b1bf44c1a4fdcc6
kstambaugh <refinancethekraken@gmail.com>
2022-04-05 18:32:14 -0400

    added new js practice

--- javaScriptReview.js
+++ javaScriptReview.js
@@ -1,5 +1,22 @@
-const food = ["hamburger","banana","chicken"]
+// const food = ["hamburger","banana","chicken"]
 
-for(let i = 0; i < food.length; i++){
-    console.log(food[i])
-}
+// for(let i = 0; i < food.length; i++){
+//     console.log(food[i])
+// }
+
+// for(i of food){
+//     console.log("2",i)
+// }
+
+// let balance = 10000
+// let byChance = 100
+// while(balance > 0){
+//     if(byChance%2){
+//     balance -= 500  //balance = balance-500
+    
+//     }else{
+//     balance -= 1000
+//     }
+//     byChance -=1
+//     console.log(balance)
+// }
